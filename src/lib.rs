a23686f8cf646a1585662df6f23d8503bdac22ee
Alvaro Polo <apoloval@gmail.com>
2015-09-15 19:22:38 +0200

    Implement basic module layout
    Including logging configuration.

---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+
+//
+// FlightVars
+// Copyright (c) 2015 Alvaro Polo
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#[macro_use]
+extern crate log;
+
+extern crate libc;
+extern crate log4rs;
+
+use std::mem::size_of;
+use std::ptr;
+
+use libc::malloc;
+
+mod logging;
+
+struct Module;
+
+impl Module {
+    pub fn new() -> Self { Module }
+    pub fn start(&mut self) {
+        logging::config_logging();
+        info!("Starting FlightVars module v{}", FLIGHTVARS_VERSION);
+        info!("FlightVars module started successfully");
+    }
+    pub fn stop(self) {
+        info!("Stopping FlightVars module");
+        info!("FlightVars module stopped successfully");
+    }
+}
+
+const FLIGHTVARS_VERSION: &'static str = "0.1.0";
+
+static mut MODULE: *mut Module = 0 as *mut Module;
+
+#[export_name="\x01_DLLStart"]
+pub extern "stdcall" fn dll_start() {
+    unsafe {
+        MODULE = malloc(size_of::<Module>() as u32) as *mut Module;
+        ptr::write(MODULE, Module::new());
+        (*MODULE).start();
+    }
+}
+
+#[export_name="\x01_DLLStop"]
+pub extern "stdcall" fn dll_stop() {
+    unsafe {
+        let m = ptr::read(MODULE);
+        m.stop();
+    }
+}
---
+++ src/logging.rs
@@ -0,0 +1,39 @@
+//
+// FlightVars
+// Copyright (c) 2015 Alvaro Polo
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+use std::boxed::Box;
+use std::path::Path;
+
+use log::LogLevelFilter;
+use log4rs::init_config;
+use log4rs::appender::FileAppender;
+use log4rs::config::{Appender, Config, Root};
+use log4rs::pattern::PatternLayout;
+
+pub fn config_logging() {
+    init_config(log_config()).unwrap()
+}
+
+fn log_config() -> Config {
+    let log_path = Path::new("Modules/flightvars.log");
+    let file_pattern = PatternLayout::new("%d{%Y/%m/%d %H:%M:%S.%f} - %m").unwrap();
+    let file_appender = FileAppender::builder(log_path)
+        .pattern(file_pattern)
+        .build()
+        .unwrap();
+    let main_appender = Appender::builder("main".to_string(), Box::new(file_appender))
+        .build();
+    let root = Root::builder(LogLevelFilter::Info)
+        .appender("main".to_string())
+        .build();
+    let config = Config::builder(root)
+        .appender(main_appender)
+        .build()
+        .unwrap();
+    config
+}
