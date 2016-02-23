95923952c4f16f6f4b68dda5afe1cf0cd53fcc5d
Alvaro Polo <alvaro.polo@jobandtalent.com>
2016-02-23 08:24:47 +0100

    Update copyright headers
    Since we have added code in 2016.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 
 //
 // FlightVars
-// Copyright (c) 2015 Alvaro Polo
+// Copyright (c) 2015, 2016 Alvaro Polo
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
