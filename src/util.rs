58ce3b05b4634f60d5745417a5e34f9586ff3122
Alvaro Polo <apoloval@gmail.com>
2016-03-23 11:49:25 +0100

    Add a `map()` method to `Consume` trait
    So consumers can be mapped to adapt the consumed input at need.

--- src/util.rs
+++ src/util.rs
@@ -7,12 +7,22 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 use std::io;
+use std::marker::PhantomData;
 use std::sync::mpsc;
 
 pub trait Consume {
     type Item;
     type Error;
     fn consume(&mut self, value: Self::Item) -> Result<(), Self::Error>;
+
+    fn map<T, F>(self, f: F) -> ConsumeMap<T, Self, F>
+    where F: FnMut(T) -> Self::Item, Self: Sized {
+        ConsumeMap {
+            _phantom: PhantomData,
+            consumer: self,
+            mapping: f
+        }
+    }
 }
 
 impl<T> Consume for mpsc::Sender<T> {
@@ -23,5 +33,36 @@ impl<T> Consume for mpsc::Sender<T> {
         self.send(value).map_err(|_| {
             io::Error::new(io::ErrorKind::BrokenPipe, "cannot write to mpsc::Sender")
         })
+    }
+}
+
+pub struct ConsumeMap<T, C, F> where C: Consume, F: FnMut(T) -> C::Item {
+    _phantom: PhantomData<T>,
+    consumer: C,
+    mapping: F
+}
+
+impl<T, C, F> Consume for ConsumeMap<T, C, F> where C: Consume, F: FnMut(T) -> C::Item {
+    type Item = T;
+    type Error = C::Error;
+
+    fn consume(&mut self, value: T) -> Result<(), Self::Error> {
+        let mapped = (self.mapping)(value);
+        self.consumer.consume(mapped)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::sync::mpsc;
+
+    use super::*;
+
+    #[test]
+    fn should_map_consumer() {
+        let (tx, rx) = mpsc::channel::<usize>();
+        let mut mapped = tx.map(|s: &str| s.len());
+        mapped.consume("foobar");
+        assert_eq!(rx.recv().unwrap(), 6);
     }
 }
