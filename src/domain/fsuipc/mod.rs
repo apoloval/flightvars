5ef80041272a843dc456ed1db0e3ee0fa0ded13b
Alvaro Polo <apoloval@gmail.com>
2016-03-23 11:01:55 +0100

    Provide scaffolding for FSUIPC domain based in MIO crate

--- src/domain/fsuipc/mod.rs
+++ src/domain/fsuipc/mod.rs
@@ -6,6 +6,59 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-pub mod types;
+use std::thread;
 
+use mio;
+
+use domain::types::*;
+
+pub mod types;
 pub use self::types::*;
+
+pub struct FsuipcDomain {
+    worker: thread::JoinHandle<()>,
+    tx: mio::Sender<Command>
+}
+
+impl FsuipcDomain {
+    pub fn new() -> FsuipcDomain {
+        let (worker, tx) = spawn_worker();
+        FsuipcDomain { worker: worker, tx: tx }
+    }
+
+    pub fn shutdown() {
+        // TODO: send a msg using tx and wait for worker to stop
+        unimplemented!()
+    }
+}
+
+
+pub struct FsuipcContext;
+
+impl FsuipcContext {
+    pub fn new()  -> FsuipcContext {
+        FsuipcContext
+    }
+}
+
+fn spawn_worker() -> (thread::JoinHandle<()>, mio::Sender<Command>) {
+    let event_loop = mio::EventLoop::new().unwrap();
+    let tx = event_loop.channel();
+    let worker = thread::spawn(move || {
+        let mut event_loop = event_loop;
+        let mut ctx = FsuipcContext::new();
+        event_loop.run(&mut ctx);
+    });
+    (worker, tx)
+}
+
+impl mio::Handler for FsuipcContext {
+    type Timeout = ();
+    type Message = Command;
+
+    fn ready(&mut self,
+             event_loop: &mut mio::EventLoop<FsuipcContext>,
+             token: mio::Token,
+             events: mio::EventSet) {
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ extern crate hex;
 extern crate libc;
 #[macro_use] extern crate log;
 extern crate log4rs;
+extern crate mio;
 extern crate ws2_32;
 
 mod domain;
